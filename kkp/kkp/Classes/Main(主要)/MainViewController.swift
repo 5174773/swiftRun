a7615f93c9fe210065e95e4dbdcbfbe4c934d288
Lo <5174773@qq.com>
2018-06-20 20:57:57 +0800

    添加CocoaPods

---
+++ kkp/kkp/Classes/Main(主要)/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  kkp
+//
+//  Created by Lo on 2018/6/20.
+//  Copyright © 2018年 Lo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let bgView = UIView()
+        bgView.frame = view.bounds
+
+        bgView.backgroundColor = UIColor.init(patternImage: UIImage(named: "bg.png")!)
+
+
+        view.addSubview(bgView)
+        
+        
+//        let topView = UIView()
+//        let bjViewImage = UIImage.init(named: "bg.png")
+//        // 设置 topView 的背景图片
+//        topView.layer.contents = bjViewImage?.cgImage
+//
+//        view.addSubview(topView)
+//
+        
+    }
+
+    
+}
